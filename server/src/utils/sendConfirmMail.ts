eb835f48c35df8d8bc3827b6de58db4530b7553f
Serhiy Ctrax <spoltoratsk@gmail.com>
2025-03-02 21:38:13 +0200

    rework sending confirmation mail logic

--- server/src/utils/sendConfirmMail.ts
+++ server/src/utils/sendConfirmMail.ts
@@ -1,23 +1,36 @@
-import nodemailer from "nodemailer";
+import mailjet from "node-mailjet";
 import dotenv from "dotenv";
+
 
 dotenv.config();
 
-export async function sendConfirmMail(email: string, token: string): Promise<void> {
-    const transporter = nodemailer.createTransport ({
-        service: 'gmail',
-        auth:{
-            user: process.env.GMAIL,
-            pass: process.env.GMAIL_PASSWORD, 
-        },
-    });
+export async function sendConfirmMail(email:string, token: string): Promise <void> {
+    try {
+        const mail = mailjet.apiConnect(
+            process.env.MAIL_JET_API_KEY!,
+            process.env.MAIL_JET_SECRET_KEY!
+        );
+        const req = mail.post("send",{version: "v3.1"}).request({
+            "Messages": [
+                {
+                    "From": {
+                        "Email": process.env.GMAIL,
+                        "Name": "Test name"
+                    },
+                    "To": [{
+                        "Email": email,
+                        "Name": "recep 1"
+                    }],
+                    "Subject": "Your confirmation code",
+                    "TextPart": "Dear recep 1, Thx for reg, here is ur confirm code",
+                    "HTMLPart": `<p> Please copy and paste this code: ${token}</p>`,
+                }
+            ]
+        });
 
-    const mailOptions = {
-        from: process.env.GMAIL,
-        to: email,
-        subject: "Confirm reg",
-        html: `<p>Будь ласка, скопіюйте цей код: 
-                ${token}   `
-    };
-    await transporter.sendMail(mailOptions);
+        await req;
+
+    } catch (error) {
+        throw error;
+    }
 }
